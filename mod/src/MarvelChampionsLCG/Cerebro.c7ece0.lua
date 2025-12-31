df5f398ed438c77c97b8cee54be8456be45fdcda
SwedeRacer <shawngus@gmail.com>
2025-12-31 08:05:36 -0700

    Implemented several bug fixes and tweaks

--- mod/src/MarvelChampionsLCG/Cerebro.c7ece0.lua
+++ mod/src/MarvelChampionsLCG/Cerebro.c7ece0.lua
@@ -1,5 +1,4 @@
 local deckId = 0
-local isPrivateDeck = true
 
 function onLoad()
   createUI()
@@ -20,18 +19,6 @@ function createUI()
   })
 
   self.createButton({
-    click_function = "setIsPrivateDeck",
-    function_owner = self,
-    position = {0,0.2,0.4},
-    width = 1700,
-    height = 400,
-    tooltip = "Click to toggle Private/Public deck ID",
-    label = "Private",
-    font_size = 350,
-    scale = {0.2,0.2,0.2}  
-  })
-
-  self.createButton({
     click_function = "importDeck",
     function_owner = self,
     position = {0,0.2,0.7},
@@ -49,24 +36,9 @@ function setDeckId(obj, playerColor, value, selected)
   deckId = value
 end
 
-function setIsPrivateDeck()
-  isPrivateDeck = not isPrivateDeck
-  local newLabel = isPrivateDeck and "Private" or "Public"
-
-  for k, v in pairs(self.getButtons()) do
-    if (v.label == "Private" or v.label == "Public") then
-      self.editButton({
-        label = newLabel,
-        index = v.index          
-      })
-    end
-  end
-end
-
 function importDeck(params)
   params = {
     deckId = deckId,
-    isPrivateDeck = isPrivateDeck,
     callbackFunction = "spawnDeck",
     callbackTarget = self
   }
